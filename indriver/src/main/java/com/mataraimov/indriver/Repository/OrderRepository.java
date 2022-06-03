760206b2442977dd3f93a7a26b193fc17693e491
mataraimov <73570575+mataraimov@users.noreply.github.com>
2022-06-03 11:31:08 +0600

    Pushing project source code
    NurNur

---
+++ indriver/src/main/java/com/mataraimov/indriver/Repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.mataraimov.indriver.Repository;
+
+import com.mataraimov.indriver.Entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long>{
+
+}
---
+++ indriver/src/test/java/com/mataraimov/indriver/BucketlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mataraimov.indriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
